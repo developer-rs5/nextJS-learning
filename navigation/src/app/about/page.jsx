c80f14f4a1e172a55fa89b9f8ae6de73e346419a
Developer RS | Rishabh Sharma <164728448+developer-rs5@users.noreply.github.com>
2025-11-15 18:01:02 +0530

    Update page.jsx

--- navigation/src/app/about/page.jsx
+++ navigation/src/app/about/page.jsx
@@ -1,10 +1,26 @@
+"use client"
 import React from 'react'
 import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 
 const page = () => {
+    const router = useRouter();
     return (
         <div>
             This is about page
+            <Image
+                src={"/vercel.svg"}
+                alt='kkuch nahi . '
+                width={200}
+                height={200}
+            />
+            
+
+            <button onClick={()=>router.push("https://google.com")} className='h-10 w-30 bg-blue-400 rounded-2xl hover:bg-black hover:border m-30 hover:cursor-pointer ' >
+                open google
+            </button>
+
+            
 
             <Image
                 src={"/img.jpg"}
@@ -12,6 +28,7 @@ const page = () => {
                 width={200}
                 height={200}
             />
+            
         </div>
     )
 }
